9ad5558e08f68f0851f389d2160a598b387858d3
kabo <andres200127@hotmail.com>
2025-05-08 23:40:50 -0500

    Requisiciones

--- src/controllers/requisicionController.js
+++ src/controllers/requisicionController.js
@@ -46,8 +46,18 @@ const getRequisicion = async (req, res) => {
         // Consultamos
         const searchReq = await requisicion.findByPk(reqId, {
             include: [{
-                model: cotizacion
-            }]
+                model: cotizacion,
+                include:[{
+                    model: kit,
+                    include: [{
+                        model: materia,
+                    }],
+                    through: {
+                        attributes: ['cantidad', 'precio'] // o los campos que tengas en KitCotizacion
+                    }
+                }],
+                
+            }]   
         }).catch(err => {
             console.log(err);
             return null;
@@ -56,7 +66,42 @@ const getRequisicion = async (req, res) => {
         // Validamos respuesta
         if(!searchReq) return res.status(404).json({msg: 'No hemos encontrado esta requisición'});
         // Caso contrario, la enviamos
-        res.status(200).json(searchReq);
+
+
+        const kits = searchReq.cotizacion.kits
+
+        const totalMateriaPrima  = {};
+
+        kits.forEach(kit => {
+            const cantidadKit = kit.kitCotizacion.cantidad;
+
+            kit.materia.forEach(mt => {
+                const key = `${mt.id}`
+                const cantidadTotal = Number(mt.itemKit.medida) * Number(cantidadKit);
+                
+                if (!totalMateriaPrima[key]) {
+                    totalMateriaPrima[key] = {
+                        id: mt.id,
+                        nombre: mt.description,
+                        medidaOriginal: mt.medida,
+                        unidad: mt.unidad,
+                        cantidad: 0 
+                    };
+                }
+
+                totalMateriaPrima[key].cantidad += cantidadTotal;
+            })
+
+        })
+
+        Object.values(totalMateriaPrima);
+
+        res.status(200).json({
+            requisicion: searchReq,
+            cantidades: Object.values(totalMateriaPrima)
+        });  
+
+
     }catch(err){
         console.log(err);
         res.status(500).json({msg: 'Ha ocurrido un error en la principal.'});
--- src/routes/requisicion.js
+++ src/routes/requisicion.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getAllRequisiciones } = require('../controllers/requisicionController');
+const { getAllRequisiciones, getRequisicion } = require('../controllers/requisicionController');
 const router = express.Router();
 
 
 router.route('/getAll')
     .get(getAllRequisiciones)
 
+router.route('/get/:reqId')
+    .get(getRequisicion)
 module.exports = router; 
  
